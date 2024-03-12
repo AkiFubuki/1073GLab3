4015bf8b7b7312ff253feea673852cdec59496b3
AkiFubuki <156011234+AkiFubuki@users.noreply.github.com>
2024-03-11 23:47:30 -0400

    Add files via upload

---
+++ js/oop.js
@@ -0,0 +1,35 @@
+const output = document.getElementById('output');
+const output2 = document.getElementById('output2');
+
+/* STEP 1a: Create a new object using a regular function */
+
+/* STEP 1b: Use the console to create a new person, and then invoke the function represented by .greeting() */
+
+
+// STEP 2a: Rewrite the function using constructor functions
+
+
+// STEP 2b: Create a couple of different people and invoke the greeting method
+
+
+// STEP 3a: Complete the Person constructor
+
+
+// STEP 3b: Instantiate a new Person
+
+// STEP 3c: Access various properties of person1
+
+
+// STEP 4b: Add properties and methods to the car object
+
+
+// STEP 4c: Change properties and invoke methods of car
+
+
+// STEP 5b: Output the brand property of anotherCar
+
+
+
+
+
+// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
