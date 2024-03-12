4f40127ad4cad4b626c7d9710f29c9b21a923ad4
AkiFubuki <156011234+AkiFubuki@users.noreply.github.com>
2024-03-11 23:49:23 -0400

    Add files via upload

--- js/oop.js
+++ js/oop.js
@@ -29,7 +29,75 @@ const output2 = document.getElementById('output2');
 // STEP 5b: Output the brand property of anotherCar
 
 
+// That's it! Now on to Lab 3...
+// Create a JavaScript function that permits the creation of a hamburger object
+// Consider that a hamburger has many options – that might include
+/*
+    different types of buns,
+    vegetable garnishes, 
+    assortments of cheeses, 
+    specialty sauces, 
+    different meat patties(beef, chicken, vegetarian), 
+    single, double, or triple patty, 
+    Pickles, hot peppers, olives etc.
+*/
+// add some extra if you want
+// Make sure that the function allows for the output of a sentence that describes the hamburger, too
 
+function createHamburger(options) {
+    // Default options
+    const defaultOptions = {
+        bun: 'sesame seed bun',
+        vegetables: [],
+        cheeses: [],
+        sauces: [],
+        patties: ['beef'],
+        pattyCount: 1,
+        toppings: []
+    };
+
+    // Merge default options with provided options
+    const hamburger = { ...defaultOptions, ...options };
+
+    // Function to generate description sentence
+    hamburger.describe = function() {
+        let description = `A delicious hamburger with ${this.pattyCount} ${this.pattyCount > 1 ? 'patties' : 'patty'} made of ${this.patties.join(', ')}, served on a ${this.bun}`;
+
+        if (this.vegetables.length > 0) {
+            description += `, topped with ${this.vegetables.join(', ')}`;
+        }
+
+        if (this.cheeses.length > 0) {
+            description += `, covered with ${this.cheeses.join(', ')} cheese`;
+        }
+
+        if (this.sauces.length > 0) {
+            description += `, drizzled with ${this.sauces.join(', ')} sauce`;
+        }
+
+        if (this.toppings.length > 0) {
+            description += `, garnished with ${this.toppings.join(', ')}`;
+        }
+
+        description += '. Enjoy!';
+        return description;
+    };
+
+    return hamburger;
+}
+
+// Example usage:
+const myHamburger = createHamburger({
+    bun: 'whole wheat bun',
+    vegetables: ['lettuce', 'tomato', 'onion'],
+    cheeses: ['cheddar'],
+    sauces: ['barbecue'],
+    patties: ['beef', 'chicken'],
+    pattyCount: 2,
+    toppings: ['pickles', 'jalapenos']
+});
+
+console.log(myHamburger.describe());
 
 
 // This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_JS
